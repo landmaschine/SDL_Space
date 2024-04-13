88ab479d22fa84de7624bed5ad006e817ca5b072
landmaschine <leon@echoton.com>
2024-04-13 11:38:45 +0200

    update

---
+++ build/CMakeFiles/Space.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Space.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leonw/Documents/dev/SDL_Space
+# Build directory: /home/leonw/Documents/dev/SDL_Space/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
