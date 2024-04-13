0150c2db020abdb4fa446d0a53849c64596e126a
landmaschine <leon@echoton.com>
2024-04-13 11:46:30 +0200

    s

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leonw/Documents/dev/SDL_Space
-# Build directory: /home/leonw/Documents/dev/SDL_Space/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
